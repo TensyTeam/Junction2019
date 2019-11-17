68f43ec212d309254167254f57ce574868cdcd35
mike-petrov <mis5116@yandex.ru>
2019-11-17 02:25:46 +0200

    update Story component

--- web/src/Components/StoriesList/Story/Story.jsx
+++ web/src/Components/StoriesList/Story/Story.jsx
@@ -6,11 +6,20 @@ import './Story.css';
 
 const Story = (props) => {
 	const {
-		story, onCallStory,
+		story, onLike,
 	} = props;
 	return (
-		<div className="story" key={story.id} onClick={() => { onCallStory(story.id); }}>
-			{story.text}
+		<div className="story" key={story.id}>
+			<div className="story_wrapper">
+				<video src={story.video} autoPlay controls />
+			</div>
+			<div className="story_reactions">
+				<span onClick={() => { onLike(story.id, 0); }}><img src="/img/reactions/1.png" alt="" /></span>
+				<span onClick={() => { onLike(story.id, 1); }}><img src="/img/reactions/2.png" alt="" /></span>
+				<span onClick={() => { onLike(story.id, 2); }}><img src="/img/reactions/3.png" alt="" /></span>
+				<span onClick={() => { onLike(story.id, 3); }}><img src="/img/reactions/4.png" alt="" /></span>
+				<span onClick={() => { onLike(story.id, 4); }}><img src="/img/reactions/5.png" alt="" /></span>
+			</div>
 		</div>
 	);
 };
